8fc0378c8484bcc99085aba963ec1f7fce775f7f
Alberto Sánchez <slalbertojesus@gmail.com>
2019-10-20 20:42:41 -0500

    Practica de redes

---
+++ Interface/src/Operaciones/Implementacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author Beardedfox
+ */
+
+public class Implementacion extends IOperacionesPOA{
+    
+    public Implementacion(){
+        super();
+    }
+
+    public boolean registrar(Usuario Ops) {
+        System.out.println("Hola");
+    return false;}
+
+    public boolean iniciarsesion(Usuario Ops) {
+        System.out.println("Hola");
+    return false;}
+}
